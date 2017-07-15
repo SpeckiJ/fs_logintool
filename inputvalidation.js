6cf57602c1e5e5b08b25f05e18e0f6dbb02af1df
SpeckiJ <SpeckiJ@gmail.com>
2017-07-15 22:29:46 +0200

    fixed regex for usernames

--- inputvalidation.js
+++ inputvalidation.js
@@ -3,7 +3,7 @@ const sanitizeHtml = require('sanitize-html');
 
 const checkParams = validator.isObject()
   .withRequired('name', validator.isString({
-    regex: /^(?=\w{1,50}$).+$/,
+    regex: /^(\w| *|\/){1,50}$/,
     message: 'Name musst be alphanumeric and maximum 50 characters.'
   }))
   .withRequired('party', validator.isString({
